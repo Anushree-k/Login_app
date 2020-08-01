6ff9a68d1ba1fef0a0a9b6a2b741a6ffee160988
Anushree-k <49914163+Anushree-k@users.noreply.github.com>
2020-08-01 15:16:55 +0530

    Add files via upload

---
+++ loginappindex.js
@@ -0,0 +1,69 @@
+function xyz()
+{
+    if($("#first").val()=='' || $("#last").val()=='')
+    {
+        alert("First & Last name required");
+    }
+    else
+    {
+        if($("#em2").val()=='')
+        {
+            alert("kindly put your mail id !");
+        }
+        else
+        {
+            if($("#pass1").val()=='' || $("#pass2").val()=='')
+            {
+                alert("Password area required");
+            }
+            else
+            {
+                if($("#pass1").val()!=$("#pass2").val())
+                {
+                    alert("Password does not matched");
+                }
+                else
+                {
+                    localStorage.setItem($("#em2").val(),$("#em2").val());
+                    localStorage.setItem($("#pass1").val(),$("#pass1").val());
+                    alert("sign up successfull");
+                    window.location.href="index.html";
+                }
+            }
+        }
+    }
+}
+
+function abc()
+{
+    if($("#em1").val()=='')
+    {
+        alert("Enter Email id");
+    }
+    else
+    {
+        if($("#em1").val()==localStorage.getItem($("#em1").val()))
+        {
+            if($("#pass").val()=='')
+            {
+                alert("Enter Password");
+            }
+            else
+            {
+                if($("#pass").val()==localStorage.getItem($("#pass").val()))
+                {
+                    alert("Signed in Successfully");
+                    window.location.href="welcome.html";
+                }
+                else
+                {
+                    alert("Wrong Password");
+                }
+            }
+        }
+        else
+        {
+            alert("Invalid ID");
+        }
+    }
+}
